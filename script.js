260084d72a30ac1a0eab3c39b9f35141d729abff
Alex Christenson <christenson10@gmail.com>
2021-04-15 15:45:47 -0400

    add for loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
+var question = document.querySelector('#question');
+var choices = Array.from(document.querySelectorAll('.choice-text'));
+var progressText = document.querySelector('#progressText');
+var scoreText = document.querySelector('#score');
 var startBtn = document.querySelector("#startBtn");
 
 //Write start quiz function, hide greetings div, display questions
@@ -31,46 +31,54 @@ var quiz = function() {
 //Start time
 let timeRemaining = 60;
 
-startBtn.addEventListener("click", startQuiz);
-
-// var quizQuestions = [
-//     {
-//         question:'Which of the following is NOT true about anonymous functions?',
-//         a:'They are created using the function operator or arrow syntax.',
-//         b:'They don't have a name.',
-//         c:'They are most commonly used as a callback function.',
-//         d:'They are NOT created at runtime.',
-//         correctAnswer:'d'
-//     },
-//     {
-//         question:'Which of the following is an example of something that JavaScript can add to a website?',
-//         a:'Highlight text.',
-//         b:'Create multiple paragraphs of content on a website.',
-//         c:'Animate content on the website so that it grows larger when a user clicks on it.',
-//         d:'Add a static image to a website.',
-//         correctAnswer:'c'
-//     },
-//     {
-//         question:'Which of the following statements correctly describes const?',
-//         a:'const is not used to declare variables, it is a method.',
-//         b:'const is used in ES6 to declare a variable that is block scoped and may be reassigned.',
-//         c:'const is used in ES6 to declare a variable that is block scoped and may NOT be reassigned.',
-//         d:'const is used in ES5 to declare a variable that is globally scoped and may be reassigned.',
-//         correctAnswer:'c'
-//     },
-//     {
-//         question:'What does the term Scope refer to?',
-//         a:'It refers to the naming convention of variables in a program.',
-//         b:'It defines the data type of the value that a variable contains.',
-//         c:'It refers to the length of the data that a variable contains.',
-//         d:'It refers to the visibility of variables in different contexts.',
-//         correctAnswer:'d'
-//     },
-// ];
+var questions = [
+    {
+        question:'Which of the following is NOT true about anonymous functions?',
+        answerA:'They are created using the function operator or arrow syntax.',
+        answerB:'They dont have a name.',
+        answerC:'They are most commonly used as a callback function.',
+        answerD:'They are NOT created at runtime.',
+        correctAnswer:'d'
+    },
+    {
+        question:'Which of the following is an example of something that JavaScript can add to a website?',
+        answerA:'Highlight text.',
+        answerB:'Create multiple paragraphs of content on a website.',
+        answerC:'Animate content on the website so that it grows larger when a user clicks on it.',
+        answerD:'Add a static image to a website.',
+        correctAnswer:'c'
+    },
+    {
+        question:'Which of the following statements correctly describes const?',
+        answerA:'const is not used to declare variables, it is a method.',
+        answerB:'const is used in ES6 to declare a variable that is block scoped and may be reassigned.',
+        answerC:'const is used in ES6 to declare a variable that is block scoped and may NOT be reassigned.',
+        answerD:'const is used in ES5 to declare a variable that is globally scoped and may be reassigned.',
+        correctAnswer:'c'
+    },
+    {
+        question:'What does the term Scope refer to?',
+        answerA:'It refers to the naming convention of variables in a program.',
+        answerB:'It defines the data type of the value that a variable contains.',
+        answerC:'It refers to the length of the data that a variable contains.',
+        answerD:'It refers to the visibility of variables in different contexts.',
+        correctAnswer:'d'
+    },
+];
 
 //Add questions to the page
 
 //For loop to latch onto HTML
+for(var i=0; i < questions.length; i++){
+    var response = window.prompt(question[i].prompt);
+    if (response == questions[i].answer){
+        score++;
+        alert("Correct!");
+    } else {
+        alert("Wrong!");
+ 
+    }
+}
 
 //When question is answered, move onto the next questions
 
@@ -81,3 +89,17 @@ startBtn.addEventListener("click", startQuiz);
 //Game is over when questions are over or timer reaches 0
 
 //Even listener to write initials and store in local storage
+
+$inputButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    loadScore();
+    let initials = $nameInput.value;
+    let saveObj = {
+        initials: initials,
+        score: score
+    };
+    saveFiles.push(saveObj);
+    console.log(saveFiles);
+    saveScore();
+
+startBtn.addEventListener("click", startQuiz);
