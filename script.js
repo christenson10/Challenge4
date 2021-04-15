5c3cd5e1948419d83871e0ec33b696bafba1c799
Alex Christenson <christenson10@gmail.com>
2021-04-14 23:57:06 -0400

    initial commit/psuedo

---
+++ script.js
@@ -0,0 +1,52 @@
+
+var startBtn = document.querySelector("#startBtn");
+
+//Write start quiz function, hide greetings div, display questions
+
+
+//Set interval timer
+
+
+//For loop to latch onto HTML
+
+
+//Give user space to write initials and store in local storage
+
+
+startBtn.addEventListener("click", startQuiz);
+
+
+// var quizQuestions = [
+//     {
+//         question:'Which of the following is NOT true about anonymous functions?',
+//         a:'They are created using the function operator or arrow syntax.',
+//         b:'They don't have a name.',
+//         c:'They are most commonly used as a callback function.',
+//         d:'They are NOT created at runtime.',
+//         correctAnswer:'d'
+//     },
+//     {
+//         question:'Which of the following is an example of something that JavaScript can add to a website?',
+//         a:'Highlight text.',
+//         b:'Create multiple paragraphs of content on a website.',
+//         c:'Animate content on the website so that it grows larger when a user clicks on it.',
+//         d:'Add a static image to a website.',
+//         correctAnswer:'c'
+//     },
+//     {
+//         question:'Which of the following statements correctly describes const?',
+//         a:'const is not used to declare variables, it is a method.',
+//         b:'const is used in ES6 to declare a variable that is block scoped and may be reassigned.',
+//         c:'const is used in ES6 to declare a variable that is block scoped and may NOT be reassigned.',
+//         d:'const is used in ES5 to declare a variable that is globally scoped and may be reassigned.',
+//         correctAnswer:'c'
+//     },
+//     {
+//         question:'What does the term Scope refer to?',
+//         a:'It refers to the naming convention of variables in a program.',
+//         b:'It defines the data type of the value that a variable contains.',
+//         c:'It refers to the length of the data that a variable contains.',
+//         d:'It refers to the visibility of variables in different contexts.',
+//         correctAnswer:'d'
+//     },
+// ];
