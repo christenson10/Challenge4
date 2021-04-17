f0911a7332f594819083014cf2853a52a94195ab
Alex Christenson <christenson10@gmail.com>
2021-04-17 13:34:43 -0400

    initials input/event listener

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var quizQuestions = document.getElementById('#quizQuestion');
-var choices = Array.from(document.getElementById('.choice-text'));
-var progressText = document.getElementById('#progressText');
+var questions = document.getElementById('#questions');
 var timer = document.getElementById("timer");
 var scoreText = document.getElementById('#score');
 var startBtn = document.getElementById("#startBtn");
+
+
+
+
+// var questionArr = [{question:}]
+
+var timeLeft = 60;
 
 //Display timer and start countdown when button pressed
 function startQuiz() {
@@ -39,13 +44,21 @@ function startQuiz() {
 
 // Timer and countdown function from 60, continue to show question if time is still left
 function countdown() {
-    setInterval(function()) {
-        if (time>=0) {
-            seconds = seconds--;
-        } else {
-            clearInterval("timer");
-    }, 1000)
+    var timeLeft = 60;
 
+    var timerInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timeInterval.textContent = timeLeft + ' seconds';
+            timeLeft--;
+        } else if {timeLeft === 1) {
+            timeInterval.textContent = timeLeft + ' second';
+            timeLeft--;
+        } else {
+            timeLeft.textContent = "";
+            clearInterval(timerInterval);
+            displayMessage():
+        }
+    }, 1000);
     showQuestions();
 }
 
@@ -61,25 +74,25 @@ function countdown() {
 
 
 // Quiz questions and answer
-const quizQuestions = [
+var questions = [
     {
         question:'Which of the following is NOT true about anonymous functions?',
-        options:['They are created using the function operator or arrow syntax.', 'They dont have a name.', 'They are most commonly used as a callback function.', 'They are NOT created at runtime.'] 
+        answers:['They are created using the function operator or arrow syntax.', 'They dont have a name.', 'They are most commonly used as a callback function.', 'They are NOT created at runtime.'] 
         correctAnswer: 'They are NOT created at runtime.',
     },
     {
         question:'Which of the following is an example of something that JavaScript can add to a website?',
-        options:['Highlight text.', 'Create multiple paragraphs of content on a website.', 'Animate content on the website so that it grows larger when a user clicks on it.', 'Add a static image to a website.'],
+        answers:['Highlight text.', 'Create multiple paragraphs of content on a website.', 'Animate content on the website so that it grows larger when a user clicks on it.', 'Add a static image to a website.'],
         correctAnswer:'Animate content on the website so that it grows larger when a user clicks on it.',
     },
     {
         question:'Which of the following statements correctly describes const?',
-        option:['const is not used to declare variables, it is a method.', 'const is used in ES6 to declare a variable that is block scoped and may be reassigned.', 'const is used in ES6 to declare a variable that is block scoped and may NOT be reassigned.', 'const is used in ES5 to declare a variable that is globally scoped and may be reassigned.'],
+        answers:['const is not used to declare variables, it is a method.', 'const is used in ES6 to declare a variable that is block scoped and may be reassigned.', 'const is used in ES6 to declare a variable that is block scoped and may NOT be reassigned.', 'const is used in ES5 to declare a variable that is globally scoped and may be reassigned.'],
         correctAnswer:'const is used in ES6 to declare a variable that is block scoped and may NOT be reassigned.',
     },
     {
         question:'What does the term Scope refer to?',
-        option:['It refers to the naming convention of variables in a program.', 'It defines the data type of the value that a variable contains.', 'It refers to the length of the data that a variable contains.', 'It refers to the visibility of variables in different contexts.'],
+        answers:['It refers to the naming convention of variables in a program.', 'It defines the data type of the value that a variable contains.', 'It refers to the length of the data that a variable contains.', 'It refers to the visibility of variables in different contexts.'],
         correctAnswer:'It refers to the visibility of variables in different contexts.',
     },
 ];
@@ -102,26 +115,43 @@ for(var i=0; i < questions.length; i++){
     }
 }
 
-//When question is answered, move onto the next questions
-
-//Notify when wrong answer, subtract time from the clock
-
-//Finish quiz and give final score
+//When question is answered, move onto the next questions.
+    //"onclick" function to target 
+    //Notify when wrong answer, subtract time from the clock
 
 //Game is over when questions are over or timer reaches 0
+//Finish quiz and give final score
 
-//Even listener to write initials and store in local storage
+//Local storage for initials input and list
+var initialsInput = document.getElementById("inputName");
+var initialsList = document.querySelector("showScore");
+var submit = document.getElementById()
 
-$inputButton.addEventListener("click", function(e) {
-    e.preventDefault();
-    loadScore();
-    let initials = $nameInput.value;
-    let saveObj = {
-        initials: initials,
-        score: score
-    };
-    saveFiles.push(saveObj);
-    console.log(saveFiles);
-    saveScore();
+//Event listener for high scores
+submit.addEventListener("click", function () {
+    localStorage.setItem("initialsInput", initialsInput.value);
+    highScores();
+});
+
+//Scoreboard page
+function highScores() {
+
+}
+    //Return home button
+    //Clear scores button
+
+// $inputButton.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     loadScore();
+//     let initials = $nameInput.value;
+//     let saveObj = {
+//         initials: initials,
+//         score: score
+//     };
+//     saveFiles.push(saveObj);
+//     console.log(saveFiles);
+//     saveScore();
+
+
 
 startBtn.addEventListener("click", startQuiz);
